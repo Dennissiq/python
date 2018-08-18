2e962a786f0a0113f1e989b570a6fb5b1d989fec
Dennis Siqueira <ti.dennissiqueira@gmail.com>
2018-08-18 19:24:25 -0300

    l33t sem for

--- Aula 1/l33t.py
+++ Aula 1/l33t.py
@@ -1,15 +1,5 @@
-text = input("Digite seu texto: ")
+import string
 
-leetmsg = text
+text = input('Digite seu texto:\n')
 
-leetwords = "aeiots"
-for letter in text:
-    if letter in leetwords:
-        leetmsg = leetmsg.replace('a', str(4))
-        leetmsg = leetmsg.replace('e', str(3))
-        leetmsg = leetmsg.replace('i', '!')
-        leetmsg = leetmsg.replace('o', str(0))
-        leetmsg = leetmsg.replace('t', str(7))
-        leetmsg = leetmsg.replace('s', '$')
-
-print("Seu texto l33t é:", leetmsg.upper())
+print(text.translate(bytes.maketrans(b'aeiots',b'43!07$')))
